77ba97881ec00865499a034b8002714c13ca6046
Harsh Raj <203050059@iitb.ac.in>
2021-08-21 02:41:25 +0530

    updated

--- parameters.py
+++ parameters.py
@@ -5,6 +5,9 @@ class SimulatorParameter():
             -global_time: Tracks global time
         '''
         self.N = None
+        self.z = None
+        self.Tmean = []
+        self.Kmean = []
         self.global_time = 0
         pass
     def populateParams(self,path):
@@ -14,4 +17,25 @@ class SimulatorParameter():
             self.Tmean = Third Line: contains self.N float numbers which is mean time for generating the Tnx for each peer
             self.Kmean = Fourth Line: contains self.N float numbers which is mean time for generating the Block for each peer
         '''
-        
+        file = open(path,"r")
+        count = 1
+        for line in file:
+            if count==1:
+                self.N = int(line)
+                count +=1
+                continue
+            elif count==2:
+                self.z = int(line)
+                count +=1
+                continue
+            elif count==3:
+                mean = line.split()
+                self.Tmean = [int(x) for x in mean]
+                count +=1
+                continue
+            elif count==4:
+                mean = line.split()
+                self.Kmean = [int(x) for x in mean]
+                break
+        pass
+            
